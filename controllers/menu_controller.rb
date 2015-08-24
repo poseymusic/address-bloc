539326141521d89d5a2fae2dde27bba23d0ea207
Ken Posey <kennethwposey@yahoo.com>
2015-08-23 22:25:45 -0700

    Checkpoint-23-Address-finish

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -55,6 +55,7 @@ class MenuController
     system "clear"
     puts "End of entries"
   end
+
   def create_entry
     system "clear"
     puts "New AddressBloc Entry"
@@ -69,21 +70,91 @@ class MenuController
     puts "New entry created"
   end
 
-  def search_entries
-
-  end
-
   def read_csv
     #enter File
     # get chomp.
     # pass name to
-    @address_book.import_from_csv(file_name)
-    #add more
-    #quit
-  end
+    # #1
+    print "Enter CSV file to import: "
+    file_name = gets.chomp
+    # #2
+    if file_name.empty?
+      system "clear"
+      puts "No CSV file read"
+      main_menu
+    end
+    begin
+      #add more
+      #quit
+      entry_count = @address_book.import_from_csv(file_name).count
+      system "clear"
+      puts "#{entry_count} new entries added from #{file_name}"
+    rescue
+      puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+      read_csv
+    end
 
-  def  entry_submenu(entry)
-    puts "n - next entry"
+    def delete_entry(entry)
+      @address_book.entries.delete(entry)
+      puts "#{entry.name} has been deleted"
+    end
+
+    def edit_entry(entry)
+      print "Updated name: "
+      name = gets.chomp
+      print "Updated phone number: "
+      phone_number = gets.chomp
+      print "Updated email: "
+      email = gets.chomp
+      entry.name = name if !name.empty?
+      entry.phone_number = phone_number if !phone_number.empty?
+      entry.email = email if !email.empty?
+      system "clear"
+      puts "Updated entry:"
+      puts entry
+    end
+  end
+####################################
+  def search_entries
+    print "Search by name: "
+    name = gets.chomp
+    match = @address_book.binary_search(name)
+    system "clear"
+    if match
+      puts match.to_s
+      search_submenu(match)
+    else
+      puts "No match found for #{name}"
+    end
+  end
+#####################################
+  def search_submenu(entry)
+    puts "\nd - delete entry"
+    puts "e - edit this entry"
+    puts "m - return to main menu"
+    selection = gets.chomp
+    case selection
+    when "d"
+      system "clear"
+      delete_entry(entry)
+      main_menu
+    when "e"
+      edit_entry(entry)
+      system "clear"
+      main_menu
+    when "m"
+      system "clear"
+      main_menu
+    else
+      system "clear"
+      puts "#{selection} is not a valid input"
+      puts entry.to_s
+      search_submenu(entry)
+    end
+  end
+#####################################
+  def entry_submenu(entry)
+    puts "\nn - next entry"
     puts "d - delete entry"
     puts "e - edit this entry"
     puts "m - return to main menu"
@@ -91,14 +162,17 @@ class MenuController
     case selection
       when "n"
       when "d"
+        delete_entry(entry)
       when "e"
+        edit_entry(entry)
+        entry_submenu(entry)
       when "m"
         system "clear"
         main_menu
       else
         system "clear"
         puts "#{selection} is not a valid input"
-        entries_submenu(entry)
+        entry_submenu(entry)
       end
-  end
+    end
 end
--- models/address_book.rb
+++ models/address_book.rb
@@ -29,7 +29,6 @@ class AddressBook
       row_hash = row.to_hash
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
-
   end
 
   def binary_search(name)
@@ -77,5 +76,4 @@ class AddressBook
       index += 1
     end
   end
-
 end
